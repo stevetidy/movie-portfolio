222295f04be2bebbc20f333db0b90568a99232fd
Steve Tidy <stevetidy@gmail.com>
2026-09-01 21:26:13 -0400

    Made text more legible

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -38,37 +38,40 @@ export default async function MovieDetailsPage({ params }: PageProps) {
         )}
 
         <div className={styles['details__container']}>
-          <div className={styles['details__poster-wrapper']}>
-            <img
-              src={posterUrl}
-              alt={movie.title}
-              className={styles['details__poster']}
-            />
-          </div>
-
-          <div className={styles['details__content']}>
-            <h1 className={styles['details__title']}>{movie.title}</h1>
-            {movie.tagline && (
-              <p className={styles['details__tagline']}>"{movie.tagline}"</p>
-            )}
-
-            <div className={styles['details__meta']}>
-              <span>⭐ {(movie.vote_average ?? 0).toFixed(1)}</span>
-              <span>{movie.runtime} min</span>
-              <span>{movie.release_date?.split('-')[0]}</span>
+          {/* Frosted Glass Card Wrapper */}
+          <div className={styles['details__card']}>
+            <div className={styles['details__poster-wrapper']}>
+              <img
+                src={posterUrl}
+                alt={movie.title}
+                className={styles['details__poster']}
+              />
             </div>
 
-            <WatchlistButton movie={movie} />
+            <div className={styles['details__content']}>
+              <h1 className={styles['details__title']}>{movie.title}</h1>
+              {movie.tagline && (
+                <p className={styles['details__tagline']}>"{movie.tagline}"</p>
+              )}
 
-            <div className={styles['details__genres']}>
-              {movie.genres.map((genre) => (
-                <span key={genre.id} className={styles['details__genre-tag']}>
-                  {genre.name}
-                </span>
-              ))}
+              <div className={styles['details__meta']}>
+                <span>⭐ {(movie.vote_average ?? 0).toFixed(1)}</span>
+                <span>{movie.runtime} min</span>
+                <span>{movie.release_date?.split('-')[0]}</span>
+              </div>
+
+              <WatchlistButton movie={movie} />
+
+              <div className={styles['details__genres']}>
+                {movie.genres.map((genre) => (
+                  <span key={genre.id} className={styles['details__genre-tag']}>
+                    {genre.name}
+                  </span>
+                ))}
+              </div>
+
+              <p className={styles['details__overview']}>{movie.overview}</p>
             </div>
-
-            <p className={styles['details__overview']}>{movie.overview}</p>
           </div>
         </div>
       </article>
