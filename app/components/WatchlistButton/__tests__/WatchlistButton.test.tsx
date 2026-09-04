34074dcc433b23a95969a601e34bc162567ad87c
Steve Tidy <stevetidy@gmail.com>
2026-09-03 20:29:59 -0400

    Added jest testing capabilities

---
+++ app/components/WatchlistButton/__tests__/WatchlistButton.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+// Simple placeholder test to verify Jest is working
+describe('Sanity Check', () => {
+  it('true should be true', () => {
+    expect(true).toBe(true);
+  });
+});
